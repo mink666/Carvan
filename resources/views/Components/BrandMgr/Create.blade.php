565f769ce161323ff59ada213baa8284e91ecaf2
Zemasis <vhp28022000@gmail.com>
2025-05-18 00:13:50 +0700

    CRUFix

--- resources/views/Components/BrandMgr/Create.blade.php
+++ resources/views/Components/BrandMgr/Create.blade.php
@@ -43,13 +43,13 @@
                 <!-- Brand Name -->
                 <div>
                     <label class="block text-base font-medium text-gray-700 mb-1">Name</label>
-                    <input type="text" name="name" readonly class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black">
+                    <input type="text" name="name"  class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black">
                 </div>
 
                 <!-- Range -->
                 <div>
-                    <label class="block text-base font-medium text-gray-700 mb-1">Range</label>
-                    <input type="text" name="Year" readonly class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black">
+                    <label class="block text-base font-medium text-gray-700 mb-1">Company_full_name</label>
+                    <input type="text" name="company_full_name"  class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black">
                 </div>
                 <!-- Description -->
                 <div>
--- resources/views/Components/RangesMgr/Edit.blade.php
+++ resources/views/Components/RangesMgr/Edit.blade.php
@@ -1,19 +1,15 @@
 <div class="max-w-8xl w-full mx-auto bg-white shadow-xl rounded-xl overflow-hidden flex">
 
-    <form action="" method="POST" enctype="multipart/form-data" class="flex w-full">
+    <form action="{{ route('Admin.RangesMgr.update',['id' => $range->id]) }}" method="POST" enctype="multipart/form-data" class="flex w-full">
         @csrf
+        @method('PUT')
         <div class="w-1/2 p-6 flex flex-col">
             <h2 class="text-lg font-semibold text-gray-700 mb-4">Update Range</h2>
 
             <div class="flex-grow p-2 border border-dashed border-gray-300 rounded-md bg-white flex flex-col items-center ">
                 <!-- Preview image -->
-                <img id="previewImage" src="{{ asset('images/placeholder.png') }}" alt="Preview" class="w-80 h-80 object-contain rounded-md" />
+                <img id="previewImage" src="{{ asset('images/RangeofCarOverView.jpg') }}" alt="Preview" class="w-130 h-130 object-contain rounded-md" />
 
-                {{-- Upload Image --}}
-                <div class="w-full">
-                    <label class="block text-sm font-medium text-gray-700 mb-2">Upload Car Image</label>
-                    <input id="imageInput" type="file" name="image" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#f1912b]">
-                </div>
             </div>
         </div>
         <!-- Bên phải: Form nhập thông tin -->
@@ -63,20 +59,3 @@
     </form>
 </div>
 
-
-<script>
-    document.getElementById('imageInput').addEventListener('change', function(event) {
-        const file = event.target.files[0]; // Lấy file được chọn
-        const previewImage = document.getElementById('previewImage'); // Lấy thẻ img để hiển thị preview
-
-        if (file) {
-            const reader = new FileReader(); // Tạo FileReader để đọc file
-            reader.onload = function(e) {
-                previewImage.src = e.target.result; // Gán src của ảnh bằng kết quả đọc file
-            };
-            reader.readAsDataURL(file); // Đọc file dưới dạng Data URL
-        } else {
-            previewImage.src = "{{ asset('images/placeholder.png') }}"; // Nếu không có file, hiển thị ảnh mặc định
-        }
-    });
-</script>
